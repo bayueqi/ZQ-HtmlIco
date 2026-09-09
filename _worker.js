2c4612cc2a53acb9eb6f37e33478de41aa320620
八月琪 <zouqi050813@gmail.com>
2026-09-09 19:20:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -843,7 +843,7 @@ function getFrontendHTML(PASSWORD) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <title>ZQ-HtmlIco</title>
-    <link rel="icon" href="https://img.520jacky.dpdns.org/i/2026/04/14/937761.webp" type="image/webp">
+    <link rel="icon" href="https://img.520jacky.dpdns.org/i/2026/03/23/849814.svg" type="image/webp">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
     <style>
